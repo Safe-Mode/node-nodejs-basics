2a8f56ea4a84944fe6118ead7335a2514ab48bc0
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-02 00:20:52 +0400

    create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import * as fs from 'node:fs/promises';
+
+const FRESH_STR = 'I am fresh and young';
+const ERROR_MSG = 'FS operation failed';
+const Name = {
+    DIR: 'files',
+    FRESH_FILE: 'fresh.txt'
+};
+
 const create = async () => {
-    // Write your code here 
+    const path = `./${Name.DIR}/${Name.FRESH_FILE}`;
+
+    try {
+        await fs.access(path);
+        console.error(new Error(ERROR_MSG));
+    } catch {
+        await fs.mkdir('./files', { recursive: true });
+        await fs.writeFile(path, FRESH_STR);
+    }
 };
 
 await create();
