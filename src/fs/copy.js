f25152a8b49bfe164ade730059f6d31726369784
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-02 21:07:05 +0400

    copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { access, cp } from 'node:fs/promises';
+
+const ERROR_MSG = 'FS operation failed';
+const Name = {
+    DIR: 'files',
+    DIR_COPY: 'files_copy'
+};
+
 const copy = async () => {
-    // Write your code here 
+    try {
+        await access(Name.DIR);
+        await access(Name.DIR_COPY);
+        cp(Name.DIR, Name.DIR_COPY, { recursive: true });
+    } catch {
+        console.error(new Error(ERROR_MSG));
+    }
 };
 
 copy();
