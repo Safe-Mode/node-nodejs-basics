63afb9928d5db14a7aa70fb280cfcc7f4b600cd7
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-02 22:27:55 +0400

    delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { access, rm } from 'node:fs/promises';
+
+const ENDPOINT = './src/fs';
+const ERROR_MSG = 'FS operation failed';
+const Name = {
+    DIR: 'files',
+    REMOVE_FILE: 'fileToRemove.txt',
+};
+const Path = {
+    removeFile: `${ENDPOINT}/${Name.DIR}/${Name.REMOVE_FILE}`
+};
+
 const remove = async () => {
-    // Write your code here 
+    try {
+        await access(Path.removeFile);
+        rm(Path.removeFile);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
 };
 
 await remove();
