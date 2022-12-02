0b9f1ef150bd4989a448d940b1b1819bdb889ae7
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-02 21:26:08 +0400

    Refactor variables

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,16 +1,21 @@
 import { access, cp } from 'node:fs/promises';
 
 const ERROR_MSG = 'FS operation failed';
+const ENDPOINT = './src/fs';
 const Name = {
     DIR: 'files',
     DIR_COPY: 'files_copy'
 };
+const Path = {
+    dir: `${ENDPOINT}/${Name.DIR}`,
+    dirCopy: `${ENDPOINT}/${Name.DIR_COPY}`
+};
 
 const copy = async () => {
     try {
-        await access(Name.DIR);
-        await access(Name.DIR_COPY);
-        cp(Name.DIR, Name.DIR_COPY, { recursive: true });
+        await access(Path.dir);
+        await access(Path.dirCopy);
+        cp(Path.dir, Path.dirCopy, { recursive: true });
     } catch {
         console.error(new Error(ERROR_MSG));
     }
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,21 +1,25 @@
-import * as fs from 'node:fs/promises';
+import { access, mkdir, writeFile } from 'node:fs/promises';
 
 const FRESH_STR = 'I am fresh and young';
 const ERROR_MSG = 'FS operation failed';
+const ENDPOINT = './src/fs';
 const Name = {
     DIR: 'files',
     FRESH_FILE: 'fresh.txt'
 };
+const Path = {
+    dir: `${ENDPOINT}/${Name.DIR}`,
+};
 
 const create = async () => {
-    const path = `./${Name.DIR}/${Name.FRESH_FILE}`;
+    const path = `${Path.dir}/${Name.FRESH_FILE}`;
 
     try {
-        await fs.access(path);
+        await access(path);
         console.error(new Error(ERROR_MSG));
     } catch {
-        await fs.mkdir('./files', { recursive: true });
-        await fs.writeFile(path, FRESH_STR);
+        await mkdir(`${Path.dir}`, { recursive: true });
+        await writeFile(path, FRESH_STR);
     }
 };
 
