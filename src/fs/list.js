0ac4545997bd6fd2ff9ea76b89555e3b162e136c
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-02 23:28:24 +0400

    list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { access, readdir } from 'node:fs/promises';
+
+const ENDPOINT = './src/fs';
+const ERROR_MSG = 'FS operation failed';
+const Name = {
+    DIR: 'files'
+};
+const PATH = `${ENDPOINT}/${Name.DIR}`;
+
 const list = async () => {
-    // Write your code here 
+    try {
+        await access(PATH);
+        const files = await readdir(PATH);
+        console.log(files);
+    } catch {
+        throw new Error(ERROR_MSG);
+    }
 };
 
 await list();
