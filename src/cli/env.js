65df4462bd3fc31c6ad307c06527b4d43608c666
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-03 17:44:14 +0400

    env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,16 @@
+import { env } from 'node:process';
+
+const RSS_ENTRY = 'RSS_';
+
 const parseEnv = () => {
-    // Write your code here 
+    let rssStr = '';
+
+    for (let key in env) {
+        if (env.hasOwnProperty(key) && key.includes(RSS_ENTRY)) {
+            rssStr = rssStr ? `${rssStr}; ${key}=${env[key]}` : `${key}=${env[key]}`;
+        }
+    }
+    console.log(rssStr);
 };
 
 parseEnv();
