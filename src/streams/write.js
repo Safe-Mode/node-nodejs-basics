5326b4a93e2ae782a57971263fa330eac85d7b0c
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-05 01:50:59 +0400

    Streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,9 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const writableStream = createWriteStream('./src/streams/files/fileToWrite.txt');
+    stdin.pipe(writableStream);
 };
 
 await write();
