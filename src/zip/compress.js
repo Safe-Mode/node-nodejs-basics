601e9926592cf112e348f1feb232adf93b22a189
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-05 23:23:02 +0400

    zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,25 @@
+import { createGzip } from 'node:zlib';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+
+const FILES_PATH = 'src/zip/files';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream(`${FILES_PATH}/fileToCompress.txt`);
+    const dest = createWriteStream(`${FILES_PATH}/archive.gz`);
+
+    pipeline(
+        source,
+        gzip,
+        dest,
+        (err) => {
+            if (err) {
+              console.error('An error occurred:', err);
+              process.exitCode = 1;
+            }
+        }
+    );
 };
 
 await compress();
