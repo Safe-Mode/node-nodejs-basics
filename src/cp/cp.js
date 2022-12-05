699d9ae1d8b393ba942aefb4f794c3af25d318ae
Sergey Dmitriev <sergei_dmitriev1@rntgroup.com>
2022-12-06 03:56:33 +0400

    cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,9 @@
+import { spawn, execFile, fork } from 'node:child_process';
+
+const FILE_PATH = './src/cp/files/script.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(FILE_PATH, args);
 };
 
 spawnChildProcess();
